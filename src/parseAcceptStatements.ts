38a6a3ce80cb4ab4ab4b99eac72644172ab75eaa
Kei Ito <kei.itof@gmail.com>
2022-08-07 17:29:54 +0900

    feat: add parseAcceptStatement

---
+++ src/parseAcceptStatements.ts
@@ -0,0 +1,44 @@
+import {splitString} from '@nlib/typing';
+import {parseNegotiationItem} from './parseNegotiationItem';
+
+// eslint-disable-next-line @nlib/no-globals
+const {Error, Number} = globalThis;
+
+export interface ParseAcceptResult<Type extends string> {
+    value: Type,
+    q: number,
+    // parameters: Record<string, string>,
+}
+
+/**
+ * https://www.rfc-editor.org/rfc/rfc7231.html#section-5.3
+ * Parses an "Accept-*" value and yield `{value, q}`.
+ * @example
+ * ```javascript
+ * const generator = parseAcceptStatements('v1,v2;q=0.9,v3;q=0.8')
+ * generator.next().value; // → {value: 'v1', q: 1}
+ * generator.next().value; // → {value: 'v2', q: 0.9}
+ * generator.next().value; // → {value: 'v3', q: 0.8}
+ * generator.next(); // → {value: undefined, done: true}
+ * ```
+ */
+export const parseAcceptStatements = function* (
+    acceptStatements: string,
+    loose = false,
+): Generator<ParseAcceptResult<string>> {
+    for (const {value: statement} of splitString(acceptStatements, ',')) {
+        try {
+            const {value, parameters} = parseNegotiationItem(statement);
+            const q = Number.parseFloat(parameters.q || '1');
+            if (0 <= q && q <= 1) {
+                yield {value, q};
+            } else {
+                throw new Error(`InvalidQuality: ${value}`);
+            }
+        } catch (error: unknown) {
+            if (!loose) {
+                throw error;
+            }
+        }
+    }
+};
